f0135e0347149a3b948bc45865eb44a7def532c7
Jisu <jaejisu@gmail.com>
2017-03-14 15:39:22 -0700

    add news scss

--- themes/catalyst/news.php
+++ themes/catalyst/news.php
@@ -15,22 +15,20 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php while ( have_posts() ) : the_post(); ?>
+        <div class="header">
+            <h1>news & media</h1>
+            <div class="hero banner"></div>
+        </div>
 
-			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+        <div class="video"></div>
 
-			<?php the_post_navigation(); ?>
-
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
-
-		<?php endwhile; // End of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+        <div class="stories">
+            <h2>Links to other stories</h2>
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+		</main> <!--#main -->
+	</div> <!--#primary -->
 
 <?php get_footer(); ?>
