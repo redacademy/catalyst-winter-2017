9bc7e222678e138890838d5f98ea3affcf6c858e
Jisu <jaejisu@gmail.com>
2017-03-14 10:08:54 -0700

    modified header footer

--- themes/catalyst/footer.php
+++ themes/catalyst/footer.php
@@ -10,8 +10,28 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+				<div class="footer">
+					<div class="page-link">
+					<a href="">careers</a>
+					<a href="">news & media</a>
+					</div>
+
+					<div class="company-info">
+						<div class="footerLogo">
+						<img src= alt="catalyst Logo">
+						</div>
+						<h3>catalyst community development society</h3>
+						<p>(the amp) 90-425 Carrall Street, Vancouver, BC, V6B 6E3</p>
+						<p> @ 2017 All Rights Reserved</p>
+
+					</div>
+
+
+					<div class="sns-icon">
+					<span><i class="fa fa-twitter-square" aria-hidden="true"></i></span>
+					<span><i class="fa fa-facebook-square" aria-hidden="true"></i></span>
+					</div>
+
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
--- themes/catalyst/header.php
+++ themes/catalyst/header.php
@@ -21,14 +21,21 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
-				<div class="site-branding">
+				<!--<div class="site-branding">
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-				</div><!-- .site-branding -->
+				</div>-->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+					<div class="container">
+						<div class="logo">
+							<a href="<?php  echo esc_url( home_url( '/' ) ); ?>"><h1 class="header-logo">Catalyst Logo</h1></a>
+						</div>
+						<div class="menu">
+							<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu', ) ); ?>
+							<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
+						</div>
+					</div>
 				</nav><!-- #site-navigation -->
 
 			</header><!-- #masthead -->
