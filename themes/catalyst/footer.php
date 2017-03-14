ed8468a8bcb7581df9146492bd5b9a2361090dac
Jisu <jaejisu@gmail.com>
2017-03-14 12:29:29 -0700

    modified header n footer

--- themes/catalyst/footer.php
+++ themes/catalyst/footer.php
@@ -17,20 +17,18 @@
 					</div>
 
 					<div class="company-info">
-						<div class="footerLogo">
+						<div class="footer-title">
+							<p class="footerLogo"></p>
+							<h3>catalyst community development society</h3>
 						</div>
-						<h3>catalyst community development society</h3>
 						<p>(the amp) 90-425 Carrall Street, Vancouver, BC, V6B 6E3</p>
 						<p> @ 2017 All Rights Reserved</p>
-
 					</div>
-
 
 					<div class="sns-icon">
 					<span><i class="fa fa-twitter-square" aria-hidden="true"></i></span>
 					<span><i class="fa fa-linkedin-square" aria-hidden="true"></i></span>
 					</div>
-
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
--- themes/catalyst/header.php
+++ themes/catalyst/header.php
@@ -21,11 +21,6 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
-				<!--<div class="site-branding">
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-				</div>-->
-
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					<div class="container">
 						<div class="logo">
