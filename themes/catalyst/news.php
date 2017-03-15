f458462fb9e1f0e39063c8bd42d3c01da8b47a00
Jisu <jaejisu@gmail.com>
2017-03-15 11:02:27 -0700

    testing

--- themes/catalyst/news.php
+++ themes/catalyst/news.php
@@ -15,25 +15,6 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-<<<<<<< HEAD
-		<?php while ( have_posts() ) : the_post(); ?>
-
-			<?php get_template_part( 'template-parts/content', 'single' ); ?>
-
-			<?php the_post_navigation(); ?>
-
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
-
-		<?php endwhile; // End of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-=======
         <div class="header">
             <h1>news & media</h1>
             <div class="hero banner"></div>
@@ -61,6 +42,5 @@ get_header(); ?>
 		<div>
         </main> <!--#main -->
 	</div> <!--#primary -->
->>>>>>> 34779e15a6fe7d5d82175d165d64b9053e420db9
 
 <?php get_footer(); ?>
