60931159b5a293e2698ade74fc2e34555022729e
mr-moto <khash615@gmail.com>
2017-03-15 15:53:32 -0700

    structured careers page

--- themes/catalyst/archive-careers.php
+++ themes/catalyst/archive-careers.php
@@ -8,17 +8,23 @@
  get_header(); ?>
 
 <div class="page-header-container">
-	<p class="page-title">Job Opportunities</p>
+	<h2 class="page-title">Job Opportunities</h2>
 </div>
 <div class="job-container">
 	<?php while ( have_posts() ) : the_post(); ?>
 	<div class="job-post">
-		<?php echo CFS()->get( 'job_title' ); ?>  
-		<p class="role">role:</p>
-		<?php echo CFS()->get( 'role' ); ?>
-		<p class="responsibilities">responsibilities:</p>
-		<?php echo CFS()->get( 'responsibilities' ); ?>   
+		<h3 class='job-title'><?php echo CFS()->get( 'job_title' ); ?> </h3> 
+		<div class="role-container">
+			<p class="role">role:</p>
+			<?php echo CFS()->get( 'role' ); ?>
+		</div>
+		<div class="resp-container">
+			<p class="responsibilities">responsibilities:</p>
+			<?php echo CFS()->get( 'responsibilities' ); ?>   
+		</div>
+		<a href="#" class="contact-link"><div class="contact-button"><p class="contact">Contact us</p></div></a>
 	</div>
+
 	<?php endwhile; ?>
 </div>
 
