c4a2e176b24696f03670f013daf8919655fddada
Jisu <jaejisu@gmail.com>
2017-03-14 21:28:24 -0700

    add frontpage

--- themes/catalyst/front-page.php
+++ themes/catalyst/front-page.php
@@ -8,9 +8,33 @@
 get_header(); ?>
 
 
+<section class="front-page">
 
+  <div class ="home-hero"></div>
 
+    <div>
+        <div class="formula">
+            <h2>the catalyst formula</h2>
+            <div></div>
+            <a>learn more</a>
+        </div>
 
+        <div class="projects">
+            <h2>our project</h2>
+            <div></div>
+            <a>view more projects</a>
+        </div>
+
+        <div class="team">
+            <h2>team catalyst</h2>
+            <div></div>
+            <a>collaborate with us</a>
+        </div>
+    </div>
+
+        <p><a href="<?php echo get_post_type_archive_link('adventure'); ?>">learn more</a></p>
+
+</section>
 
 
 
