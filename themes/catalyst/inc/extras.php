dce0ef04ac7475d20862c185ccab6dc4a5f05a8e
Evan Gray <evanlawgray@gmail.com>
2017-03-17 12:13:53 -0700

    Fix errors in extras.php.

--- themes/catalyst/inc/extras.php
+++ themes/catalyst/inc/extras.php
@@ -21,11 +21,6 @@ function red_starter_body_classes( $classes ) {
 }
 add_filter( 'body_class', 'red_starter_body_classes' );
 
-<<<<<<< HEAD
-
-function news_hero_banner_css() {
-	if(!is_page_template('news.php')){
-=======
 function catalyst_project_css() {
     
 		$url = CFS()->get ( 'banner_image'); 
@@ -44,36 +39,28 @@ function catalyst_project_css() {
 // second version
 function our_story_hero_banner_css() {
 	if(!is_page_template('page-our-story.php')){
->>>>>>> master
 		return;
 	}
 
 	$image = CFS()->get( 'heroimage' );
-		if(!$image){
-			return;
-		}
-<<<<<<< HEAD
-	$hero_css = ".page-template-news .herobanner {
-=======
+	
+    if(!$image){
+		return;
+	}
 
 	$hero_css = ".page-template-page-our-story .herobanner {
->>>>>>> master
         background:
             linear-gradient( to bottom, rgba(0,0,0,0.4) 0%, rgba(0,0,0,0.4) 100% ),
             url({$image}) no-repeat center 100%;
         background-size: cover, cover;
 		height: 65vh;
-<<<<<<< HEAD
-}";
-	wp_add_inline_style( 'red-starter-style', $hero_css );
+    }";
 
-} add_action('wp_enqueue_scripts', 'news_hero_banner_css' );
-=======
+wp_add_inline_style( 'red-starter-style', $hero_css );
 
-}";
-	wp_add_inline_style( 'red-starter-style', $hero_css );
+} 
 
-} add_action('wp_enqueue_scripts', 'our_story_hero_banner_css' );
+add_action('wp_enqueue_scripts', 'our_story_hero_banner_css' );
 
 // Add header image for collaborate page 
 
@@ -99,4 +86,4 @@ function catalyst_collaborate_image_css() {
 }
 
 add_action('wp_enqueue_scripts', 'catalyst_collaborate_image_css');
->>>>>>> master
+
