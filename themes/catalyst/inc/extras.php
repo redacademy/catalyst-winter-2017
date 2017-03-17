e1bb0eab5d194fc0dbe6415b7dc2b45f946f59e2
Jisu <jaejisu@gmail.com>
2017-03-17 12:58:30 -0700

    fixed the errors

--- themes/catalyst/inc/extras.php
+++ themes/catalyst/inc/extras.php
@@ -21,17 +21,12 @@ function red_starter_body_classes( $classes ) {
 }
 add_filter( 'body_class', 'red_starter_body_classes' );
 
-<<<<<<< HEAD
-
-function news_hero_banner_css() {
-	if(!is_page_template('news.php')){
-=======
 function catalyst_project_css() {
-    
-		$url = CFS()->get ( 'banner_image'); 
+
+		$url = CFS()->get ( 'banner_image');
 
         $single_project_css = "
-        .single-projects .single-content .banner{ 
+        .single-projects .single-content .banner{
             background:url($url) no-repeat center bottom;
 			background-size: cover;
 			height: 375px;
@@ -44,7 +39,6 @@ function catalyst_project_css() {
 // second version
 function our_story_hero_banner_css() {
 	if(!is_page_template('page-our-story.php')){
->>>>>>> master
 		return;
 	}
 
@@ -52,36 +46,26 @@ function our_story_hero_banner_css() {
 		if(!$image){
 			return;
 		}
-<<<<<<< HEAD
-	$hero_css = ".page-template-news .herobanner {
-=======
 
 	$hero_css = ".page-template-page-our-story .herobanner {
->>>>>>> master
         background:
             linear-gradient( to bottom, rgba(0,0,0,0.4) 0%, rgba(0,0,0,0.4) 100% ),
             url({$image}) no-repeat center 100%;
         background-size: cover, cover;
 		height: 65vh;
-<<<<<<< HEAD
 }";
 	wp_add_inline_style( 'red-starter-style', $hero_css );
 
 } add_action('wp_enqueue_scripts', 'news_hero_banner_css' );
-=======
 
-}";
-	wp_add_inline_style( 'red-starter-style', $hero_css );
 
-} add_action('wp_enqueue_scripts', 'our_story_hero_banner_css' );
-
-// Add header image for collaborate page 
+// Add header image for collaborate page
 
 function catalyst_collaborate_image_css() {
 
     if ( ! is_page_template('page-templates/collaborate.php') ) {
         return;
-    } 
+    }
 
     $image = CFS()->get('heroimage');
 
@@ -99,4 +83,4 @@ function catalyst_collaborate_image_css() {
 }
 
 add_action('wp_enqueue_scripts', 'catalyst_collaborate_image_css');
->>>>>>> master
+
