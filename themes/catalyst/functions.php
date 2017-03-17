c2fb50b310bb3afa0e6825eccc78f757b16adf8d
mr-moto <khash615@gmail.com>
2017-03-17 11:13:31 -0700

    added flickity

--- themes/catalyst/functions.php
+++ themes/catalyst/functions.php
@@ -85,13 +85,25 @@ add_filter( 'stylesheet_uri', 'red_starter_minified_css', 10, 2 );
 function red_starter_scripts() {
 	wp_enqueue_style( 'red-starter-style', get_stylesheet_uri() );
 
+
+	wp_enqueue_script('flickity', get_template_directory_uri() . '/build/js/flickity.pkgd.min.js');
+	wp_enqueue_style('flickity', get_template_directory_uri() . '/build/css/flickity.min.css');
+
 	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
+
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'red_starter_scripts' );
+
+
+
+
+
+
+
 
 
 remove_filter ('the_content', 'wpautop');
