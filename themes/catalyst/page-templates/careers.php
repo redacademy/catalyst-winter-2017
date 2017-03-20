e07c5d3519bf7ad2e9207ddaf1b261905564ea2a
mr-moto <khash615@gmail.com>
2017-03-20 12:21:54 -0700

    added title, text, intro

--- themes/catalyst/page-templates/careers.php
+++ themes/catalyst/page-templates/careers.php
@@ -8,11 +8,24 @@
 
 get_header(); ?>
 <div class="hero-image-banner"> </div>
+<header class="entry-header">
+	<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	<p class="banner-text">
+		<?php echo CFS()->get( 'bannertext' ); ?>
+	</p>
+</header><!-- .entry-header -->
+<p class="intro-copy">
+	<?php echo CFS()->get( 'intro_copy' ); ?>
+</p>
 <div class="page-header-container">
 	<h2 class="page-title">Job Opportunities</h2>
 </div>
 <div class="job-container">
-	<?php while ( have_posts() ) : the_post(); ?>
+	 <?php $posts = get_posts( array( 
+        'post_type' => 'projects', 
+        'order' => 'ASC', 
+    ));
+    foreach ( $posts as $post ) : ?>
 	<div class="job-post">
 		<h3 class='job-title'><?php echo CFS()->get( 'job_title' ); ?> </h3> 
 		<div class="role-container">
@@ -25,7 +38,7 @@ get_header(); ?>
 		</div>
 		<a href="#" class="contact-link">Contact us</a>
 	</div>
-	<?php endwhile; ?>
+	<?php endforeach; ?>
 </div>
 
 <?php get_footer(); ?>
