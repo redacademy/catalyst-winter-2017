85b8c8ff040bbb2b00d9cf146a5211cf08f2d99c
Evan Gray <evanlawgray@gmail.com>
2017-03-20 17:55:41 -0700

    Rought out show popout function.

--- themes/catalyst/js/main.js
+++ themes/catalyst/js/main.js
@@ -18,4 +18,19 @@
         });
     });
 
+    // Toggle profile popout when enter/exit buttons are clicked (Our Team page)
+
+    var $staffEnterButtons = $( '.staff-enter-button' );
+    var $staffProfilePreviews = $( '.staff-profile-preview' );
+    var $staffProfilePopouts = $( '.staff-profile-popout' );
+    
+    $staffEnterButtons.click(function(){
+        var buttonIndex = jQuery.inArray(this, $staffEnterButtons);
+        console.log(buttonIndex);
+        
+        $staffProfilePreviews.toggleClass( 'hide-preview' );
+        var thisPopout = $staffProfilePopouts[buttonIndex];
+        thisPopout.classList.remove( 'profile-popout' );
+    });
+
 })(jQuery);
