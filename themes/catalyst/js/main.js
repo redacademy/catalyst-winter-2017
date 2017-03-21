e5d9bc2d7cf42a60ab8a7b828490cbbf61e3e691
Evan Gray <evanlawgray@gmail.com>
2017-03-21 11:23:18 -0700

    Add toggling functionality to board profiles and fix some bugs and styling.

--- themes/catalyst/js/main.js
+++ themes/catalyst/js/main.js
@@ -24,9 +24,9 @@
     var $staffEnterButtons = $( '.staff-enter-button' );
     var $boardEnterButtons = $( '.board-enter-button' );
     var $staffExitButton = $( '.staff-exit-button' );
+    var $boardExitButtons = $( '.board-exit-button' );
     
     $staffExitButton.hide();
-    // var $boardExitButton = $( '.board-exit-button' );
 
     var $staffProfilePreviews = $( '.staff-profile-preview' );
     var $boardProfilePreviews = $( '.board-profile-preview' );
@@ -38,7 +38,7 @@
     var $staffProfilesHeader = $( '.staff-profiles-wrapper > .small-header:nth-child(1)' );
 
     var $boardProfilesWrapper = $( '.board-profiles-wrapper' );
-    // var $boardProfilesHeader = $( '.board-profiles-wrapper > .small-header:nth-child(1)' );
+    var $boardProfilesHeader = $( '.board-profiles-wrapper > .small-header:nth-child(1)' );
 
     var $advisorsProfilesWrapper = $( '.advisors-profiles-wrapper' );
     var $introCopy = $( '.intro-copy' );
@@ -48,18 +48,25 @@
         var thisPopout = $staffProfilePopouts[buttonIndex];
         thisPopout = $( thisPopout );
 
+        // Grey-out the rest of the page content.
+
         $boardProfilesWrapper.animate({opacity: 0.25}, 130);
         $advisorsProfilesWrapper.animate({opacity: 0.25}, 130);
         $introCopy.animate({opacity: 0.25}, 130);
+
+        // Clear the active profiles section of inactive profiles and show only the active profile popout.
         
         $staffProfilesHeader.hide();
         $staffProfilePreviews.hide();
+        $staffProfilePopouts.hide();
         $staffProfilesWrapper.css('margin-top', '-0.75rem');
-        thisPopout.toggleClass( 'show-popout' );
+        thisPopout.show().toggleClass( 'show-popout' );
         $staffExitButton.show(130);
 
         $staffExitButton.click(function () {
-            thisPopout.hide();
+            $staffExitButton.hide();
+            thisPopout.removeClass( 'show-popout' );
+            $staffProfilesWrapper.css('margin-top', '0');
             $staffProfilePreviews.show();
             $staffProfilesHeader.show();
             $boardProfilesWrapper.css( 'opacity', '1' );
@@ -71,15 +78,33 @@
     $boardEnterButtons.click(function(){
         var buttonIndex = jQuery.inArray( this, $boardEnterButtons );
         var thisPopout = $boardProfilePopouts[buttonIndex];
+        var thisExitButton = $boardExitButtons[buttonIndex];
         thisPopout = $( thisPopout );
+        thisExitButton = $( thisExitButton );
+
+         // Grey-out the rest of the page content.
 
         $staffProfilesWrapper.animate({opacity: 0.25}, 130);
         $advisorsProfilesWrapper.animate({opacity: 0.25}, 130);
         $introCopy.animate({opacity: 0.25}, 130);
-        
-        $( '.board-profiles-wrapper > .small-header:nth-child(1)' ).hide();
-        $boardProfilePreviews.toggleClass( 'hide' );
-        thisPopout.toggleClass( 'show-popout' );
+    
+        // Clear the active profiles section of inactive profiles and show only the active profile popout.
+
+        $boardProfilesHeader.addClass( 'blue-background' ).css('margin-bottom', '0');
+        $boardProfilePreviews.hide();
+        $boardProfilePopouts.hide();
+        thisPopout.show().toggleClass( 'show-popout' );
+        thisExitButton.show();
+
+        thisExitButton.click(function(){
+            thisExitButton.hide();
+            thisPopout.removeClass( 'show-popout' );
+            $boardProfilesHeader.show();
+            $staffProfilesWrapper.css( 'opacity', '1' );
+            $advisorsProfilesWrapper.css( 'opacity', '1' );
+            $introCopy.css( 'opacity', '1' );
+            $boardProfilePreviews.show();
+        });
     });
 
 })(jQuery);
