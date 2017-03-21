440c0a9dc130dfa2fb0a5c021809cb6ce76389ae
Jisu <jaejisu@gmail.com>
2017-03-21 15:06:15 -0700

    tried to add link from post

--- themes/catalyst/page-templates/news.php
+++ themes/catalyst/page-templates/news.php
@@ -31,18 +31,25 @@ get_header(); ?>
 			$args = array(
 			'post_type' => 'newsmedia_type',
 			'order' => 'ASC',
-			'numberposts' => 5 );
+			'numberposts' => 5
+			);
 			$newsmedia_article = get_posts( $args ); // returns an array of posts
 		?>
 		<?php foreach ( $newsmedia_article as $newsmedia ) : setup_postdata( $newsmedia ); ?>
-			<div class="<?php echo $newsmedia_article->name ?>">
-       	 	<?php echo get_the_post_thumbnail( $newsmedia ) ?>
-            <?php get_template_part( 'template-parts/content', 'newsmedia_type' ); ?>
+			<div class="<?php echo $newsmedia->ID ?>">
+       	 		<?php echo get_the_post_thumbnail( $newsmedia ) ?>
+				<p><?php echo  get_the_date("Y-m-d", $newsmedia ) ?></p>
+				<h3><?php echo get_the_title( $newsmedia ) ?></h3>
+	            <?php get_template_part( 'template-parts/content', 'newsmedia_type'); ?>
+				<a class="article-link" href="<?php  echo CFS()->get( 'newslink' ); ?>">read more</a>
+				<button><?php  echo CFS()->get('newslink'); ?></button>
+
 			</div>
         <?php endforeach; wp_reset_postdata(); ?>
-	</div>
 
+	</div>
 </div>
+
 		<div class="load-more"><a>load more</a></div>
         </main> <!--#main -->
 	</div> <!--#primary -->
