8cd758d920b879f36de60ebf246212726f4df1f5
Evan Gray <evanlawgray@gmail.com>
2017-03-20 23:40:41 -0700

    Enqueue main.js again (reverting last commit).

--- themes/catalyst/functions.php
+++ themes/catalyst/functions.php
@@ -91,7 +91,7 @@ function red_starter_scripts() {
 	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
 
 	wp_enqueue_script( 'jquery');
-	wp_enqueue_script( 'red_comments', get_template_directory_uri() . 'build/js/main.min.js', array('jquery'), false, true);
+	wp_enqueue_script( 'red_comments', get_template_directory_uri() . '/js/main.js', array('jquery'), false, true);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
