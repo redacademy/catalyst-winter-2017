1a89d936794fbe2f16b294084da296e58525610f
Jisu <jaejisu@gmail.com>
2017-03-21 11:28:09 -0700

    undo

--- themes/catalyst/js/main.js
+++ themes/catalyst/js/main.js
@@ -1,20 +1,20 @@
 (function ($) {
 
-    $('.cross').hide();
-    $('.mobile-menu').hide();
-    $('.hamburger').click(function () {
-        $('.mobile-menu').slideToggle('slow', function () {
-            $('.hamburger').hide();
-            $('.cross').show();
-            $('.list').show().css('width', '400px');
+    $(".cross").hide();
+    $(".mobile-menu").hide();
+    $(".hamburger").click(function () {
+        $(".mobile-menu").slideToggle("slow", function () {
+            $(".hamburger").hide();
+            $(".cross").show();
+            $(".list").show().css("width", "400px");
         });
     });
 
-    $('.cross').click(function () {
-        $('.mobile-menu').slideToggle('slow', function () {
-            $('.cross').hide();
-            $('.hamburger').show();
-            $('.list').hide();
+    $(".cross").click(function () {
+        $(".mobile-menu").slideToggle("slow", function () {
+            $(".cross").hide();
+            $(".hamburger").show();
+            $(".list").hide();
         });
     });
 
