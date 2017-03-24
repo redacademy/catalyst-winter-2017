043fa27407f0aac71c7139cadec48ab67334ccd1
Jisu <jaejisu@gmail.com>
2017-03-23 23:32:50 -0700

    modified hero css

--- themes/catalyst/template-parts/hero-banner.php
+++ themes/catalyst/template-parts/hero-banner.php
@@ -3,7 +3,7 @@
     <div class="hero-image-banner">
     <div class="header-text">
     <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-    <p class="bannertext"><?php echo CFS()->get( 'bannertext' ); ?></p>
+    <p class="banner-text"><?php echo CFS()->get( 'bannertext' ); ?></p>
     </div>
     </div>
 </header>
