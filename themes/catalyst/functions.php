87f36b6afd644ab48d8e913d7a572fb3c751d1d8
mr-moto <khash615@gmail.com>
2017-03-24 11:39:34 -0700

    main.js fix

--- themes/catalyst/functions.php
+++ themes/catalyst/functions.php
@@ -92,14 +92,15 @@ add_filter( 'stylesheet_uri', 'catalyst_minified_css', 10, 2 );
  */
 function catalyst_scripts() {
 	wp_enqueue_style( 'catalyst-style', get_stylesheet_uri() );
+	wp_enqueue_script( 'jquery');
 
 	wp_enqueue_script('catalyst-flickity', get_template_directory_uri() . '/lib/flickity/flickity.pkgd.min.js');
 	wp_enqueue_style('catalyst-flickity', get_template_directory_uri() . '/lib/flickity/flickity.min.css');
 
 	wp_enqueue_style( 'fontAwesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', array(), '4.4.0');
 	wp_enqueue_script( 'catalyst-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
-
-	wp_enqueue_script( 'jquery');
+	
+	wp_enqueue_script( 'catalyst', get_template_directory_uri() . '/build/js/main.min.js', array('jquery'), false, true);
 	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
