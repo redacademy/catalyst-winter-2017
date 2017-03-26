c852afdcea6209d8ee649612074e2ad4c70a2009
Evan Gray <evanlawgray@gmail.com>
2017-03-25 22:28:02 -0700

    Append 5 stories to news grid on button click.

---
+++ themes/catalyst/template-parts/content-newsmedia.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template part for displaying posts.
+ *
+ * @package Catalyst_Theme
+ */
+
+?>
+	<div class="entry-content">
+		<?php the_excerpt(); ?>
+	</div><!-- .entry-content -->
