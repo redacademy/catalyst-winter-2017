4e3c0dec895b8b6c79b2c46fcf31a836035bb4c8
Jisu <jaejisu@gmail.com>
2017-03-26 17:36:25 -0700

    add non-required custom fields condition

--- themes/catalyst/template-parts/content-board.php
+++ themes/catalyst/template-parts/content-board.php
@@ -2,9 +2,17 @@
 
 	<section class="board-profile-preview profile-preview">
 		<img class="profile-image" src="<?php echo CFS()->get('profile_image'); ?>"/>
+		<?php $name = CFS()->get( 'name' );
+		if ( !empty( $name ) ) :?>
+		<h4 class="name"><?php echo $name; ?></h4>
+		<?php endif; ?>
 
-		<h4 class="name"><?php echo CFS()->get('name'); ?></h4>
-		<p class="job-title"><?php echo CFS()->get('job_title'); ?></p>
+		<?php $job_title = CFS()->get( 'job_title' );
+		if ( !empty( $job_title ) ) :?>
+		<p class="job-title"><?php echo $job_title; ?></p>
+		<?php endif; ?>
+		<!--<h4 class="name"><?php echo CFS()->get('name'); ?></h4>
+		<p class="job-title"><?php echo CFS()->get('job_title'); ?></p>-->
 		<button class="board-enter-button enter-button" type="button"><span>+</span></button>
 	</section>
 
@@ -12,19 +20,39 @@
 
 			<div class="left-column">
 				<img class="profile-image" src="<?php echo CFS()->get('profile_image'); ?>"/>
+					<?php $name = CFS()->get( 'name' );
+					if ( !empty( $name ) ) :?>
+					<h4 class="name"><?php echo $name; ?></h4>
+					<?php endif; ?>
 
-				<h4 class="name"><?php echo CFS()->get('name'); ?></h4>
-				<p class="job-title"><?php echo CFS()->get('job_title'); ?></p>
+					<?php $job_title = CFS()->get( 'job_title' );
+					if ( !empty( $job_title ) ) :?>
+					<p class="job-title"><?php echo $job_title; ?></p>
+					<?php endif; ?>
+				<!--<h4 class="name"><?php echo CFS()->get('name'); ?></h4>
+				<p class="job-title"><?php echo CFS()->get('job_title'); ?></p>-->
 
-				<p class="quote"><?php echo CFS()->get('quote'); ?></p>
+					<?php $quote = CFS()->get( 'quote' );
+					if ( !empty( $quote ) ) :?>
+					<p class="quote"><?php echo $quote; ?></p>
+					<?php endif; ?>
+				<!--<p class="quote"><?php echo CFS()->get('quote'); ?></p>-->
 			</div>
 
 			<div class="right-column">
-				<h3 class="tagline"><?php echo CFS()->get('tagline'); ?></h3>
+				<!--<h3 class="tagline"><?php echo CFS()->get('tagline'); ?></h3>-->
+				<?php $tagline = CFS()->get( 'tagline' );
+				if ( !empty( $tagline ) ) :?>
+				<h3 class="tagline"><?php echo $tagline; ?></h3>
+				<?php endif; ?>
 
-				<article class="cv">
+				<?php $cvdescription = CFS()->get( 'cvdescription' );
+				if ( !empty( $cvdescription ) ) :?>
+				<article class="cv"><?php echo $cvdescription; ?></article>
+				<?php endif; ?>
+				<!--<article class="cv">
 					<?php echo CFS()->get('cvdescription'); ?>
-				</article>
+				</article>-->
 			</div>
 
 	</section><!-- #post-## -->
--- themes/catalyst/template-parts/content-staff.php
+++ themes/catalyst/template-parts/content-staff.php
@@ -3,8 +3,15 @@
 	<section class="staff-profile-preview profile-preview">
 		<img class="profile-image" src="<?php echo CFS()->get('profile_image'); ?>"/>
 
-		<h4 class="name"><?php echo CFS()->get('name'); ?></h4>
-		<p class="job-title"><?php echo CFS()->get('job_title'); ?></p>
+		<?php $name = CFS()->get( 'name' );
+		if ( !empty( $name ) ) :?>
+		<h4 class="name"><?php echo $name; ?></h4>
+		<?php endif; ?>
+
+		<?php $job_title = CFS()->get( 'job_title' );
+		if ( !empty( $job_title ) ) :?>
+		<p class="job-title"><?php echo $job_title; ?></p>
+		<?php endif; ?>
 		<button class="staff-enter-button enter-button" type="button"><span>+</span></button>
 	</section>
 
@@ -13,19 +20,33 @@
 			<div class="left-column">
 				<img class="profile-image" src="<?php echo CFS()->get('profile_image'); ?>"/>
 
-				<h4 class="name"><?php echo CFS()->get('name'); ?></h4>
-				<p class="job-title"><?php echo CFS()->get('job_title'); ?></p>
+				<?php $name = CFS()->get( 'name' );
+				if ( !empty( $name ) ) :?>
+				<h4 class="name"><?php echo $name; ?></h4>
+				<?php endif; ?>
 
-				<p class="quote"><?php echo CFS()->get('quote'); ?></p>
+				<?php $job_title = CFS()->get( 'job_title' );
+				if ( !empty( $job_title ) ) :?>
+				<p class="job-title"><?php echo $job_title; ?></p>
+				<?php endif; ?>
+
+				<?php $quote = CFS()->get( 'quote' );
+				if ( !empty( $quote ) ) :?>
+				<p class="quote"><?php echo $quote; ?></p>
+				<?php endif; ?>
 			</div>
 
 			<div class="right-column">
+				<?php $tagline = CFS()->get( 'tagline' );
+				if ( !empty( $tagline ) ) :?>
+				<h3 class="tagline"><?php echo $tagline; ?></h3>
+				<?php endif; ?>
 
-				<h3 class="tagline"><?php echo CFS()->get('tagline'); ?></h3>
+				<?php $cvdescription = CFS()->get( 'cvdescription' );
+				if ( !empty( $cvdescription ) ) :?>
+				<article class="cv"><?php echo $cvdescription; ?></article>
+				<?php endif; ?>
 
-				<article class="cv">
-					<?php echo CFS()->get('cvdescription'); ?>
-				</article>
 			</div>
 
 	</section><!-- #post-## -->
