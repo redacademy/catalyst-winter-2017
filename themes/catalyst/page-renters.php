438add0139bd74e0793db64e704167c4c2863e99
mr-moto <khash615@gmail.com>
2017-06-23 01:51:30 -0700

    front page text width fix. close #224

--- themes/catalyst/page-renters.php
+++ themes/catalyst/page-renters.php
@@ -1,10 +1,144 @@
 <?php
 
 /**
-	* Template Name: Projects
+	* Template Name: Renters
 	*
 	* @package Catalyst Theme
 	*/
 
 get_header(); ?>
+
+<section class="banner">
+    <div>
+        <img src="" alt="">
+    </div>
+</section>
+<section class="about">
+    <div class="blue-background-header">
+        <h2 class='subheader-sm'>About Project</h2>
+    </div>
+    <div class="wrapper">
+        <div class="about-text">
+            <p>Bacon ipsum dolor amet filet mignon picanha bresaola turducken turkey landjaeger pork chop capicola t-bone meatloaf rump. Chuck bresaola cupim tail frankfurter t-bone, picanha corned beef turducken. Venison kevin doner salami alcatra pancetta meatloaf shankle cow picanha brisket meatball pork short loin andouille. Drumstick cow ball tip beef ribs. Pork chop salami flank rump pork belly, porchetta boudin shankle sausage short loin capicola andouille kielbasa.</p>
+        </div>
+    </div>
+</section>
+<section class="floorplan">
+    <div class="blue-background-header">
+        <h2 class='subheader-sm'>Floor Plans</h2>
+    </div>
+    <div class="wrapper">
+        <div class="floorplan-container">
+            <div class="floorplan-selection">
+                <div>
+                    <p>Studio</p>
+                </div>
+                <div>
+                    <p>Apartment - 1 bedroom</p>
+                </div>
+                <div>
+                    <p>Apartment - 2 bedroom</p>
+                </div>
+                <div>
+                    <p>Townhouse - 3 bedroom</p>
+                </div>
+                <div>
+                    <p>Townhouse - 3 bedroom</p>
+                </div>
+            </div>
+            <div class="floorplan-img-container">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+            </div>
+        </div>
+        <div class="floorplan-text">
+            <p>Bacon ipsum dolor amet filet mignon picanha bresaola turducken turkey landjaeger pork chop capicola t-bone meatloaf rump. Chuck bresaola cupim tail frankfurter t-bone, picanha corned beef turducken. Venison kevin doner salami alcatra pancetta meatloaf shankle cow picanha brisket meatball pork short loin andouille. Drumstick cow ball tip beef ribs. Pork chop salami flank rump pork belly, porchetta boudin shankle sausage short loin capicola andouille kielbasa.</p>
+        </div>
+    </div>
+</section>
+<section class="rent">
+    <div class="blue-background-header">
+        <h2 class='subheader-sm'>Rent</h2>
+    </div>
+    <div class="wrapper">
+        <div class="rent-text">
+            <p>Bacon ipsum dolor amet filet mignon picanha bresaola turducken turkey landjaeger pork chop capicola t-bone meatloaf rump. Chuck bresaola cupim tail frankfurter t-bone, picanha corned beef turducken. Venison kevin doner salami alcatra pancetta meatloaf shankle cow picanha brisket meatball pork short loin andouille.</p>
+        </div>
+        <div class="rent-chart-container">
+            <div class="rent-row row-head">
+                <ul>
+                    <li>Home Type</li>
+                    <li>Home Size</li>
+                    <li>Max Annual Income(2016)*</li>
+                    <li>Rent Range</li>
+                </ul>
+            </div>
+            <div class="rent-row">
+                <ul>
+                    <li>Studio</li>
+                    <li>244 – 293 sf</li>
+                    <li>$29,500</li>
+                    <li>$675.00 – $737.50</li>
+                </ul>
+            </div>
+            <div class="rent-row">
+                <ul>
+                    <li>Apartment 1 Bd</li>
+                    <li>420 – 444 sf</li>
+                    <li>$34,500</li>
+                    <li>$850.00 – $862.50</li>
+                </ul>
+            </div>
+            <div class="rent-row">
+                <ul>
+                    <li>Apartment 1 Bd</li>
+                    <li>420 – 444 sf</li>
+                    <li>$34,500</li>
+                    <li>$850.00 – $862.50</li>
+                </ul>
+            </div>
+            <div class="rent-row">
+                <ul>
+                    <li>Apartment 1 Bd</li>
+                    <li>420 – 444 sf</li>
+                    <li>$34,500</li>
+                    <li>$850.00 – $862.50</li>
+                </ul>
+            </div>
+            <div class="rent-row">
+                <ul>
+                    <li>Townhouse 4 bd</li>
+                    <li>1009 – 1141 sf</li>
+                    <li>$67,000</li>
+                    <li>$1500.00 – $1675.00</li>
+                </ul>
+            </div>
+        </div>
+        <div class="fineprint">
+            <p>*Gross Household Income</p>
+        </div>
+        <div class="more-info">
+            <h3>Further Information</h3>
+            <p>Madrona will be a completely smoke-free environment (including outdoor areas). Vehicle parking is very limited.</p>
+        </div>
+    </div>
+</section>
+<section class="contact">
+    <div class="blue-background-header">
+        <h2 class='subheader-sm'>Contact</h2>
+    </div>
+    <div class="wrapper">
+        <div class="contact-text">
+            <p>Bacon ipsum dolor amet filet mignon picanha bresaola turducken turkey landjaeger pork chop capicola t-bone meatloaf rump. Chuck bresaola cupim tail frankfurter t-bone, picanha corned beef turducken. Venison kevin doner salami alcatra pancetta meatloaf shankle cow picanha brisket meatball pork short loin andouille.</p>
+        </div>
+        <div class="contact-form">
+
+        </div>
+    </div>
+</section>
+
+
 <?php get_footer(); ?>
