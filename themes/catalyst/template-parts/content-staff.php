9f06ce8b360ebd0adf6188b4728a9cefafcc7638
Evan Gray <evanlawgray@gmail.com>
2017-07-02 21:44:38 -0700

    Style staff popout quotation marks and text on our team page.

--- themes/catalyst/template-parts/content-staff.php
+++ themes/catalyst/template-parts/content-staff.php
@@ -9,9 +9,9 @@
 	</section>
 
 	<section class="staff-profile-popout profile-popout">
-    
+
 			<?php $cvDescription = wp_kses_post( CFS()->get('cvdescription') );
-				if ( !empty( $cvDescription ) ) : 
+				if ( !empty( $cvDescription ) ) :
 			?>
 
 				<div class="left-column">
@@ -20,20 +20,23 @@
 					<h4 class="name"><?php echo esc_html( CFS()->get('name') ); ?></h4>
 					<p class="job-title"><?php echo esc_html( CFS()->get('job_title') ); ?></p>
 
-					<?php $quote = esc_html( CFS()->get( 'quote' ) ); 
-						if ( !empty( $quote ) ) : 
+					<?php $quote = esc_html( CFS()->get( 'quote' ) );
+						if ( !empty( $quote ) ) :
 					?>
-						<p class="quote">
-							<?php echo $quote; ?>
-							<div class="left-quotation"></div>
-							<div class="right-quotation"></div>
-						</p>
-						
+						<div class="quote-container">
+							<p class="quote">
+								<?php echo $quote; ?>
+								<!--<div class="left-quotation"></div>
+								<div class="right-quotation"></div>-->
+							</p>
+								<div class="left-quotation"></div>
+								<div class="right-quotation"></div>
+						</div>
 					<?php endif; ?>
 				</div>
 
 			<?php $cvDescription = wp_kses_post( CFS()->get('cvdescription') );
-				elseif ( empty( $cvDescription ) ) : 
+				elseif ( empty( $cvDescription ) ) :
 			?>
 				<div class="center-column">
 					<img class="profile-image" src="<?php echo esc_url( CFS()->get('profile_image') ); ?>"/>
@@ -41,8 +44,8 @@
 					<h4 class="name"><?php echo esc_html( CFS()->get('name') ); ?></h4>
 					<p class="job-title"><?php echo esc_html( CFS()->get('job_title') ); ?></p>
 
-					<?php $quote = esc_html( CFS()->get('quote') ); 
-						if ( !empty( $quote ) ) : 
+					<?php $quote = esc_html( CFS()->get('quote') );
+						if ( !empty( $quote ) ) :
 					?>
 						<p class="quote">
 							<?php echo $quote; ?>
@@ -57,7 +60,7 @@
 			<h3 class="tagline clearfix"><?php echo esc_html( CFS()->get('tagline') ); ?></h3>
 
 			<?php $cvDescription = wp_kses_post( CFS()->get('cvdescription') );
-				if ( !empty( $cvDescription ) ) : 
+				if ( !empty( $cvDescription ) ) :
 			?>
 
 			<article class="cv">
