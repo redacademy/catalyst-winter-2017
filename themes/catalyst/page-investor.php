9f6f70475cb43c58d348c27581e298456fe80016
Jisu <jaejisu@gmail.com>
2017-07-05 18:27:58 -0700

    addcreate investor page and nonnon-profit page and add new  fonts

---
+++ themes/catalyst/page-investor.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+	* Template Name: Non-profits page
+	*
+	* @package Catalyst Theme
+	*/
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+<?php get_template_part( 'template-parts/hero-banner'); ?>
+
+		<div class="blue-background-header">
+			<h2 class="subheader-lrg"><?php echo esc_html(CFS()->get( 'main_title' ) ); ?></h2>
+		</div>
+		<div><?php echo CFS()->get( 'main_copy' ); ?></div>
+
+		<div class="blue-background-header">
+			<h2 class="subheader-lrg"><?php echo esc_html(CFS()->get( 'second_title' ) ); ?></h2>
+		</div>
+		<div><?php echo CFS()->get( 'second_copy' ); ?></div>
+
+		<div class="blue-background-header">
+			<h2 class="subheader-lrg"><?php echo esc_html(CFS()->get( 'third_title' ) ); ?></h2>
+		</div>
+			<div><?php echo CFS()->get( 'third_copy' ); ?></div>
+
+
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
