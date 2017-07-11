f0a9c7ff07fc3ca0680fb1758d3b635c866710b6
mr-moto <khash615@gmail.com>
2017-07-11 02:47:01 -0700

    Remove location and colon from project location on frontpage and projects archive page. close #256

--- themes/catalyst/page-projects.php
+++ themes/catalyst/page-projects.php
@@ -23,9 +23,7 @@ get_header(); ?>
                 if ( !empty( $project_name ) ) : ?>
                 <h2 class="subheader-name"><?php echo $project_name; ?></h2>
                 <?php endif; ?>
-                <span class='break'>|</span>
-
-                <h2 class='subheader-sub'>Location:
+                <h2 class='subheader-sub'>
                  <?php $project_location = esc_html( CFS()->get( 'project_location' ) );
                 if ( !empty( $project_location ) ) : ?>
                 <span><?php echo $project_location; ?></span>
