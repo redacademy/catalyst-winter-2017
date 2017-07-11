614c3de5a7438985568c4aa16bdd28b8a143298c
mr-moto <khash615@gmail.com>
2017-07-11 05:20:30 -0700

    removed team carousel on front page. close #258

--- themes/catalyst/front-page.php
+++ themes/catalyst/front-page.php
@@ -59,16 +59,6 @@
             <p class="team-catalyst-copy"><?php echo $team_catalyst_copy; ?></p>
             <?php endif; ?>
 
-            <div class="member-carousel">
-                <?php $members = CFS()->get('team_members');
-                    foreach ( $members as $member ) : setup_postdata( $member ); ?>
-                    <div class="member">
-                        <img class="member_img" src='<?php echo esc_attr( $member['member'] ); ?>' />
-                        <p class="member-name"> <?php echo esc_html( $member['member_name'] ); ?> </p>
-                        <p class="position"> <?php echo esc_html( $member['position'] ); ?> </p>
-                    </div>
-                <?php endforeach; wp_reset_postdata(); ?>
-            </div>
             <a class="navigation-button" href="<?php echo get_home_url(); ?>/our-team">Meet Our Team</a>
         </div>
     </div>
