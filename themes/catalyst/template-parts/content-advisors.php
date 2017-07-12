ca6298ded5b928109c6a3550e5793661c4fd52b2
Evan Gray <evanlawgray@gmail.com>
2017-07-12 00:13:50 -0700

    Refactor advisors custom fields.

--- themes/catalyst/template-parts/content-advisors.php
+++ themes/catalyst/template-parts/content-advisors.php
@@ -1,6 +1,17 @@
 <?php ?>
 
 	<section class="profile-preview">
-		<h4 class="name"><?php echo esc_html( CFS()->get('name') ); ?></h4>
-		<p class="job-title"><?php echo esc_html( CFS()->get('job_title') ); ?></p>
+		<a href="<?php echo esc_attr( CFS()->get( 'web_site_address' ) ); ?>">
+			<h4 class="name"><?php echo esc_html( CFS()->get('name') ); ?></h4>
+
+			<?php $role = esc_html( CFS()->get( 'role' ) );
+			if( !empty( $role ) ) : ?>
+				<p class="job-title"><?php echo $role; ?></p>
+			<?php endif; ?>
+
+			<?php $organization = esc_html( CFS()->get( 'organization' ) );
+			if( !empty( $organization ) ) : ?>
+				<p class="job-title"><?php echo $organization; ?></p>
+			<?php endif; ?>
+		</a>
 	</section>
